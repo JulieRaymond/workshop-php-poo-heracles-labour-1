fa2361d26344c855e3e0979ca3ecfadbe1857933
Julie <raymondjulie@proton.me>
2023-10-10 10:24:51 +0200

    "exercice fait"

--- index.php
+++ index.php
@@ -1,4 +1,38 @@
-<?php
+    <?php
+    require "src/Fighter.php";
 
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+    $héraclès = new Fighter("Héraclès 🧔", 20, 6);
+    $lionDeNemee = new Fighter("Lion de Némée 🦁", 11, 13);
+
+    //afficher nom et points de vie:
+    // echo $héraclès->getName() . " a " . $héraclès->getLife() . " points de vie 💙" . PHP_EOL;
+    // echo $lionDeNemee->getName() . " a " . $lionDeNemee->getLife() . " points de vie 💙" . PHP_EOL;
+    //appeler une fois la fonction fight : 
+    //$héraclès->fight($lionDeNemee);
+
+    //COMBAT A MORT BOUCLE :
+    $round = 0;
+
+    while ($héraclès->isAlive() && $lionDeNemee->isAlive()) {
+        // Affichage des tours:
+        $round = $round + 1;
+        echo "Round 🕛 n° " . $round . PHP_EOL;
+
+        // Tour de Héraclés:
+        $héraclès->Fight($lionDeNemee);
+
+        // Vérification si Lion de Némée est encore en vie sinon affichage vainceur/perdant
+        if (!$lionDeNemee->isAlive()) {
+            echo $héraclès->getName() . " gagne le combat et " . $lionDeNemee->getName() . " perd." . PHP_EOL;
+            break;
+        }
+
+        // Tour de Lion de Némée:
+        $lionDeNemee->Fight($héraclès);
+
+        // Vérification si Héraclès est encore en vie sinon affichage vainceur/perdant
+        if (!$héraclès->isAlive()) {
+            echo $lionDeNemee->getName() . " gagne le combat et " . $héraclès->getName() . " perd." . PHP_EOL;
+            break;
+        }
+    }
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,57 @@
 <?php
 
-/* Fighter class definition */
+class Fighter
+{
+    public const MAX_LIFE = 100;
+
+    private string $name;
+    private int $strength;
+    private int $dexterity;
+    private int $life = self::MAX_LIFE;
+
+    public function __construct(string $name, int $strength, int $dexterity)
+    {
+        $this->name = $name;
+        $this->strength = $strength;
+        $this->dexterity = $dexterity;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getLife(): int
+    {
+        return $this->life;
+    }
+
+    public function Fight(Fighter $opponent)
+    {
+        // Calcule les dégâts de l'attaquant (nombre aléatoire entre 1 et sa force)
+        $damage = rand(1, $this->strength);
+
+        // Calcule la défense de l'attaqué (soustrait la dextérité de l'attaqué aux dégâts)
+        $defense = $opponent->dexterity;
+
+        // Calcule les dégâts nets (dégâts de l'attaquant - défense de l'attaqué)
+        $netDamage = $damage - $defense;
+
+        // Vérifie que les dégâts nets ne sont pas négatifs
+        $netDamage = max($netDamage, 0);
+
+        // Met à jour les points de vie de l'attaqué en soustrayant les dégâts nets
+        $opponent->life -= $netDamage;
+
+        // Assure que la vie de l'attaqué ne tombe pas en dessous de zéro
+        $opponent->life = max($opponent->life, 0);
+
+        echo $this->name . " attaque " . $opponent->name . " et lui inflige " . $netDamage . " points de dégâts." . PHP_EOL;
+        echo "Il reste à " . $opponent->name . " " . $opponent->life . " points de vie 💙." . PHP_EOL;
+    }
+
+    public function isAlive(): bool
+    {
+        return $this->life > 0;
+    }
+}
